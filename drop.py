119a3afb218b89fd65aeeae43b1a008e8ed816c9
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-02 14:40:07 -0400

    Rain
    All rain drops work

--- drop.py
+++ drop.py
@@ -4,20 +4,17 @@ import pygame
 class Drop:
     def __init__(self):
         self.x = random.randint(0, pygame.display.get_surface().get_size()[0])
-        self.y = -100 #random.randint(pygame.display.get_surface().get_size()[1] - 100, pygame.display.get_surface().get_size()[1])
+        self.y = random.randint(-500, -10)
         self.len_ = random.randint(5, 10)/2
-        self.speed = random.randint(1,4) / 10
+        self.speed = random.randint(5,10) / 2
 
     def fall(self):
         self.y += self.speed
 
         if self.y > pygame.display.get_surface().get_size()[1]:
-            self.y = -100 #random.randint(pygame.display.get_surface().get_size()[1] - 100, pygame.display.get_surface().get_size()[1])
+            self.y = random.randint(-200, -10)
+            self.x = random.randint(0, pygame.display.get_surface().get_size()[0])
+            self.speed = random.randint(5,10) / 2
 
     def show(self, win):
         pygame.draw.line(win, (138, 42, 240), (self.x, self.y), (self.x, self.y + self.len_))
-
-    def doctor(self):
-        print('X: ', self.x)
-        print('Y: ', self.y)
-        print('speed: ', self.speed)
--- main.py
+++ main.py
@@ -4,28 +4,32 @@ from drop import Drop
 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1200,600))
 clock = pygame.time.Clock()
 
-RED = (255,0,0)
+RED = (20,0,0)
+NUM_OF_DROPS = 1200
 
-pygame.display.set_caption('This was just a test')
+pygame.display.set_caption('I only want to see you bathing in the purple rain')
 
-
-test = Drop()
+all_drops = []
+for i in range(NUM_OF_DROPS):
+    all_drops.append(Drop())
 
 run = True
 while run:
-    print((pygame.display.get_surface().get_size())[0])
-    test.fall()
-    test.show(win)
-    test.doctor()
+
+    for i in range(NUM_OF_DROPS):
+        all_drops[i].fall()
+        all_drops[i].show(win)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
-    win.fill((0,0,0))
+    win.fill(RED)
 
 
 pygame.quit()
