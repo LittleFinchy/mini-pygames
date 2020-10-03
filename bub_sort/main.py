e3107bf364c16c0f4a51ceee61788128c0176fcc
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-03 14:42:01 -0400

    bub sort update
    added animation
    added color to show moving line

--- bub_sort/main.py
+++ bub_sort/main.py
@@ -6,7 +6,7 @@ from stick import Stick
 pygame.init()
 
 win = pygame.display.set_mode((1200,600))
-NUM_OF_STICKS = 300
+NUM_OF_STICKS = 200
 stick_spacing = pygame.display.get_surface().get_size()[0] // NUM_OF_STICKS
 
 sticks = []
@@ -15,18 +15,23 @@ for i in range(NUM_OF_STICKS):
 
 run = True
 while run:
-    
+
     for i in range(len(sticks)):
+        #sticks[-i].color = (50,205,50)
         for j in range(len(sticks) - i - 1):
-            sticks[i].show(win)
             if sticks[j] > sticks[j+1]:
-                #sticks[i].color = sticks[i+1].color = (50,205,50)
+                sticks[j+1].color = (50,205,50)
                 sticks[j].h, sticks[j+1].h = sticks[j+1].h, sticks[j].h
 
+                for stick in sticks:
+                    stick.show(win)
+                pygame.display.update()
+                win.fill((0,0,0))
+                sticks[j+1].color = (155,0,0)
 
-    print('did sort')
-    # for stick in sticks:
-    #     stick.show(win)
+
+    for stick in sticks:
+        stick.show(win)
 
 
     for event in pygame.event.get():
