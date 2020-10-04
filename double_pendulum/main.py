fbbd492d2949dea55a28520478db7152c283f9ea
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-04 00:50:24 -0400

    dub pundulum update
    string object working well
    update and show methods work
    
    next need to add gravity

--- double_pendulum/main.py
+++ double_pendulum/main.py
@@ -2,33 +2,40 @@ import pygame
 from pygame.locals import *
 from string import String
 
-
 pygame.init()
 
 win = pygame.display.set_mode((600,600))
 
 mid = pygame.display.get_surface().get_size()[0] // 2
 r1 = 200
-r2 = 200
-a1 = 0
-a2 = 0
-m1 = 10
-m2 = 10
+r2 = 150
+a1 = 0 #3.14/6
+a2 = 0 #-3.14/4
+m1 = 25
+m2 = 25
 
-line1 = String(mid, 50, r1, a1)
+line_1 = String(mid, 50, r1, a1, m1)
+line_2 = String(line_1.x2, line_1.y2, r2, a2, m2)
 
 
 run = True
 while run:
 
-    pygame.draw.line(win, line1.color, line1.point1, line1.point2)
+    line_1.show(win)
+    line_2.show(win)
+    line_1.update()
+    line_2.update(line_1)
+
+    # line_1.a = pygame.mouse.get_pos()[0] / 100
+    # line_2.a = pygame.mouse.get_pos()[1] / 100
 
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
     win.fill((0,0,0))
+
 
 
 pygame.quit()
--- double_pendulum/string.py
+++ double_pendulum/string.py
@@ -1,11 +1,13 @@
+import pygame
 import math
 
 class String:
-    def __init__(self, x, y, r, a):
+    def __init__(self, x, y, r, a, m):
         self.x1 = x
         self.y1 = y
         self.r = r
         self.a = a
+        self.m = m
         self.point1 = [self.x1, self.y1]
 
         self.x2 = int(self.r * math.sin(self.a)) + self.x1
@@ -13,3 +15,18 @@ class String:
         self.point2 = [self.x2, self.y2]
 
         self.color = (255,255,255)
+
+    
+    def show(self, win):
+        pygame.draw.line(win, self.color, self.point1, self.point2)
+        pygame.draw.circle(win, self.color, self.point2, self.m//4)
+
+    def update(self, slave=None):
+        if slave:
+            self.x1 = slave.x2
+            self.y1 = slave.y2
+            self.point1 = [self.x1, self.y1]
+
+        self.x2 = int(self.r * math.sin(self.a)) + self.x1
+        self.y2 = int(self.r * math.cos(self.a)) + self.y1
+        self.point2 = [self.x2, self.y2]
