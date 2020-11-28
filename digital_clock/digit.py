3b9b611c4c96197bd262aed5b3d5ede63eb143e2
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-11-27 22:55:39 -0500

    Update digit.py

--- digital_clock/digit.py
+++ digital_clock/digit.py
@@ -1,9 +1,14 @@
 import pygame
+from digit import Digit
 
 
 class Digit:
     def __init__(self, segments):
         self.segments = segments
+
+    def build(self, x, y):
+        for i in range(5):
+            self.segments.append(Digit())
 
     def update(self):
         pass
@@ -11,3 +16,13 @@ class Digit:
     def show(self, win):
         for seg in self.segments:
             seg.show(win)
+
+'''
+seg1 = Segment(40,40, upright=True)
+seg2 = Segment(60,20)
+seg3 = Segment(160,40, upright=True)
+seg4 = Segment(60,140)
+seg5 = Segment(160,160, upright=True)
+seg6 = Segment(40,160, upright=True)
+seg7 = Segment(60,260)
+'''
